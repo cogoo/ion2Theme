38ff230a7d695e31eba1d31760f1e064ef9eaf77
Colin Ogoo <c_ogoo@Colins-MacBook-Pro.local>
2016-11-22 00:57:52 +0000

    import new modules
    set global scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,36 @@
+import { AddArticleComponent } from './../components/add-article/add-article';
+import { ProfilePage } from './../pages/profile/profile';
+import { CategoryPage } from './../pages/category/category';
+import { FeedPage } from './../pages/feed/feed';
 import { ArticleComponent } from './../components/article/article';
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    ArticleComponent
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    ArticleComponent
-  ],
-  providers: []
+    declarations: [
+        MyApp,
+        CategoryPage,
+        ProfilePage,
+        TabsPage,
+        FeedPage,
+        ArticleComponent,
+        AddArticleComponent
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        CategoryPage,
+        ProfilePage,
+        TabsPage,
+        FeedPage,
+        ArticleComponent,
+        AddArticleComponent
+    ],
+    providers: []
 })
-export class AppModule {}
+export class AppModule { }
